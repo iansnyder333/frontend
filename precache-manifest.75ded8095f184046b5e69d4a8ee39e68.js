60851a8362ad4b6f3b0f863cc2e1d88c54213444
IanSnyder <ias20@pitt.edu>
2023-05-05 19:31:00 -0400

    Updates

---
+++ precache-manifest.75ded8095f184046b5e69d4a8ee39e68.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "31234fc1e6a9747e096e",
+    "url": "/frontend/static/css/main.4253187c.chunk.css"
+  },
+  {
+    "revision": "31234fc1e6a9747e096e",
+    "url": "/frontend/static/js/main.eadc8e0c.chunk.js"
+  },
+  {
+    "revision": "9f027b827a3f9faa92e6",
+    "url": "/frontend/static/js/runtime~main.6a7a18e7.js"
+  },
+  {
+    "revision": "efeef99474f11f6e8a43",
+    "url": "/frontend/static/css/2.b5736f75.chunk.css"
+  },
+  {
+    "revision": "efeef99474f11f6e8a43",
+    "url": "/frontend/static/js/2.ed1e086f.chunk.js"
+  },
+  {
+    "revision": "0a78b2b425ce262d37b0",
+    "url": "/frontend/static/js/3.f2d91817.chunk.js"
+  },
+  {
+    "revision": "22878c643f0e7631b453aa45fcab6f02",
+    "url": "/frontend/static/media/Snyder_Ian-42431CC.22878c64.jpg"
+  },
+  {
+    "revision": "058ce390d565f52fd486b48a39e1f046",
+    "url": "/frontend/static/media/IMG_4828.058ce390.jpeg"
+  },
+  {
+    "revision": "8da7c51fb9dad7a6c5a53ae3081188f4",
+    "url": "/frontend/static/media/db1.8da7c51f.jpeg"
+  },
+  {
+    "revision": "2f28182eae4163e5275e25753e83312d",
+    "url": "/frontend/index.html"
+  }
+];
