9a4d3ed79ed60f14a413d7e1693423f1d8ff2497
IanSnyder <ias20@pitt.edu>
2023-05-05 19:37:36 -0400

    changes

--- src/components/NavBarBasic.js
+++ src/components/NavBarBasic.js
@@ -8,7 +8,7 @@ function NavBarBasic() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Ian Snyder</Navbar.Brand>
+        <Navbar.Brand href="/">Ian Snyder</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
